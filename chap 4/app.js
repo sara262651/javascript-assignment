d19b708f4279798325c9ca27ccd1eedd016bf3bb
sara262651 <sarasajid225@gmail.com>
2024-10-12 23:22:34 +0500

    chap 4 via javascript

---
+++ chap 4/app.js
@@ -0,0 +1,24 @@
+// question 1
+let name= 'Sara' , age = 19 , city = karachi;
+
+// question 2
+// 5 legal variable names
+let firstName = "sara";
+let $age = "19";
+let _name = 'Sara';
+let last_name = 'sajid ali';
+let number2 = 20;
+
+// 5 illegal variable names
+let 2favouriteColor = 'black'
+let @age = 19;
+let last name = "sajid ali"
+let var = 'hey';
+let first-name = 'sara';
+
+
+
+// question 3
+
+let myName = "sara, 35, karachi"
+document.write (myName)
