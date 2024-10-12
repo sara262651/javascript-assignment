d61f9f495629762f871362bc07ebe0c5866bc5ea
sara262651 <sarasajid225@gmail.com>
2024-10-12 23:18:24 +0500

    chap 1 via javascript

---
+++ chap 1/app.js
@@ -0,0 +1,19 @@
+//  question 1
+alert ("Hey user! Welcome to our website");
+
+// question 2
+alert ("Error! Please enter a valid password");
+
+// question 3
+alert ("Welcome to JS Land...\nHappy Coding");
+
+// question 4
+alert ("Welcome to JS Land");
+alert ("Happy Coding\nPrevent this page from creating additional dialogs");
+
+// question 5
+alert ("Hello... I can run JS through my web browser's console")
+
+//.....question 6......
+
+
