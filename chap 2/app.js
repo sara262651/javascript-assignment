6ebd26ff80d438d74dad066e6cc8ada77b85419b
sara262651 <sarasajid225@gmail.com>
2024-10-12 23:18:58 +0500

    chap 2 via javascript

---
+++ chap 2/app.js
@@ -0,0 +1,41 @@
+// question 1
+let username;
+console.log(username)
+
+// question 2
+let myName = "sara sajid ali";
+console.log(myName)
+
+// question 3
+let message = "Hello World";
+alert (message)
+
+// question 4
+let studentName = "Ali Akbar"
+alert (studentName)
+
+let studentAge = 13;
+alert (studentAge)
+
+let studentField = "certified mobile app development";
+alert (studentField)
+
+// question 5
+let food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert (food)
+
+// question 6
+let myEmail = "sarasajid4242@gmail.com"
+alert (`my email address is ${myEmail}`)
+
+// question 7
+let book = "A smarter way to learn JavaScript"
+alert (`I am trying to learn from ${book}`)
+
+// question 8
+let javaMessage ="Yah! I can write HTML content through JAvaScript"
+document.write (javaMessage)
+
+// question 9
+let myPic = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert (myPic)
