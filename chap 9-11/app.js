a673dabb924d56984719267d4207718931f90631
sara262651 <sarasajid225@gmail.com>
2024-10-13 22:22:21 +0500

    chap 9-11 via javascript

---
+++ chap 9-11/app.js
@@ -0,0 +1,196 @@
+//...... question 1......
+// var spec = prompt("what is your city name?")
+// if(spec === "karachi"){
+//     alert("welcome to the city of lights")
+// }
+
+//......question 2.......
+// let gender = prompt("enter your gender")
+// if(gender === "male" || gender === "Male"){
+//     alert("Good morning sir")
+// }
+// else if(gender === "female" || gender === "Female"){
+//     alert("Good morning madam")
+// }
+
+//.......question 3........
+// let colorInput = prompt("enter any color from road traffic signal")
+// if(colorInput === "red" || colorInput === "Red"){
+//     alert("Must stop")
+// }
+// else if(colorInput === "Yellow" || colorInput === "yellow"){
+//     alert("Ready to move")
+// }
+// else if(colorInput === "green" || colorInput === "Green"){
+//     alert("Move now")
+// }
+
+
+
+//.......question 4.........
+// let currentFuel = prompt("enter your current fuel in litres")
+// currentFuel = parseInt(currentFuel)
+// if (currentFuel < 0.25)
+//     alert("please refill the fuel in your car")
+
+//......question 5........
+//.......part a........
+// var a = 4;
+// if(++a === 5){
+//     alert("given condition for variable a is true")
+// }
+// document.write("message displayed")
+
+//........part b........
+// var b = 82;
+// if (b++ === 83){
+//     alert("given condition for variable b is true")
+// }
+// document.write("message not displayed")
+
+//........part c........
+// var c = 12;
+// if(c++ ===13){
+//     alert("condition is true")
+// }
+// if(c === 13){
+//     alert("condition 2 is true")
+// }
+// if(++c<14){
+//     alert("condition 3 is true")
+// }
+// if(c ===14){
+//     alert ("condition 4 is true")
+// }
+// document.write("message displayed")
+
+//.........part d.........
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+//     alert("The cost equals")
+// }
+// document.write("message displayed")
+
+//........part e........
+// if(true){
+//     alert("true");
+// }
+// if(false){
+//     alert("false")
+// }
+// document.write("message displayed")
+
+//.......part f.......
+// if("car"< "cat"){
+//     alert("car is smaller than cat")
+// }
+// document.write("message displayed")
+
+//.......question 6.......
+// let obt_marks = parseInt(prompt("Enter obtained marks of your three subjects"))
+// let totalMarks = 300;
+// let per = parseInt((obt_marks/totalMarks) * 100);
+// document.write("Total Marks : " + totalMarks + "<br>")
+// document.write("Marks obtained : " + obt_marks+ "<br>")
+// document.write("Percentage : " + per +"%"+ "<br>")
+// if( per >= 80){
+//     document.write("Grade : A-one" + "<br>")
+//     document.write("Remarks : Excellent" + "<br>")
+// }
+// else if( per >= 70){
+//     document.write("Grade : A" + "<br>")
+//     document.write("Remarks : Good" + "<br>")
+// }
+// else if( per >= 60){
+//     document.write("Grade : B" + "<br>")
+//     document.write("Remarks : You need to improve" + "<br>")
+// }
+// else if( per < 60){
+//     document.write("Grade : Fail")
+//     document.write("Remarks : Sorry" + "<br>")
+// }
+
+//.......question 7.......
+// let secretNumber = 7;
+// let closerNumber = 8
+// let userInput = parseInt(prompt("Guess the secret number ranging from 1 to 10"))
+// if(userInput === secretNumber){
+//     alert("Bingo! correct answer")
+// }
+// else if(userInput === closerNumber){
+//     alert("Close enough to the correct answer")
+// }
+// else{
+//     alert("Oops! better luck next time")
+// }
+
+//.......question 8.......
+// let number = parseInt(prompt("enter any number"))
+// let remainder = numbere % 3;
+// if(rem === 0){
+//     alert("given number is divisible by 3")
+// }
+// else{
+//     alert("given number is not divisible by 3")
+// }
+
+//.......question 9......
+// let num = parseInt(prompt("Enter a number"))
+// let rem = num % 2;
+// if(rem ===0){
+//     alert("Even Number")
+// }
+// else{
+//     alert("Odd Number")
+// }
+
+//........question 10........
+// let userInput = parseInt(prompt("Enter temperature"))
+// if(userInput > 40){
+//     alert("It is too hot outside")
+// }
+// else if(userInput > 30){
+//     alert("The Weather today is Normal")
+// }
+// else if(userInput > 20){
+//     alert("Today's Weather is cool")
+// }
+// else if(userInput > 10){
+//     alert("OMG! Today's weather is so cool")
+// }
+
+//........question 11........
+// let firstNumber = parseFloat(prompt("Enter the first number:"));
+// let secondNumber = parseFloat(prompt("Enter the second number:"));
+// let operation = prompt("Enter the operation (+, -, *, /, %):");
+
+// // Initialize result variable
+// let result;
+
+// if (operation === "+") {
+//     result = firstNumber + secondNumber;
+// } else if (operation === "-") {
+//     result = firstNumber - secondNumber;
+// } else if (operation === "*") {
+//     result = firstNumber * secondNumber;
+// } else if (operation === "/") {
+//     if (secondNumber !== 0) {
+//         result = firstNumber / secondNumber;
+//     } else {
+//         result = "Error! Division by zero.";
+//     }
+// } else if (operation === "%") {
+//     result = firstNumber % secondNumber;
+// } else {
+//     result = "Invalid operation!";
+// }
+
+// // Show the result
+// alert("The result is: " + result);
+
+
+
+
+
