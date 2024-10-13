6417ade8a254197161cf91bf7a4684e665c990a0
sara262651 <sarasajid225@gmail.com>
2024-10-13 22:21:50 +0500

    chap 6-8 via javascript

---
+++ chap 6-8/app.js
@@ -0,0 +1,93 @@
+//..... question 1
+// let a = 10;
+// let num1 = ++a
+// document.write(`<br>the Value of ++a is ${num1}`)
+// let num2 = a
+// document.write(`<br>Now the Value ++a is ${num1}<br>`)
+
+// let num3 = a++ 
+// document.write(`<br>the value of a++ is ${num3}`)
+// let num4 = a
+// document.write(`<br> now the value of a++ is ${num4}<br>`)
+
+// let num5 = --a;
+// document.write(`<br>the Value of --a is ${num5}`)
+// let num6 = a;
+// document.write(`<br>now the Value of --a is ${num5}<br>`)
+
+// let num7 = a--;
+// document.write(`<br>the Value of --a is ${num7}`)
+// let num8 = a;
+// document.write(`<br>now the Value of --a is ${num8}<br>`)
+
+//question 2
+
+// var aPoint = 2;
+// var bPoint = 1;
+
+// document.write(`<br>a is ${aPoint} <br> b is ${bPoint}`)
+// let numA = --aPoint
+// let numB = numA - --bPoint
+// let numC = numB + ++bPoint
+// let numD = numC + bPoint--
+// document.write(`<br>--a is ${numA}`)
+// document.write(`<br>--a - --b is ${numB}`)
+// document.write(`<br>--a - --b + ++b is ${numC}`)
+// document.write(`<br>--a - --b + ++b + b-- is ${numD}`)
+// document.write(`<br>the Result is: ${numD}`)
+
+//......question 3.......
+
+// var ques = prompt("what is your name?")
+// alert("hello, " + ques + " Hope you have a great day")
+
+//.......question 4.........
+
+// var num = prompt("enter a number")
+// if (num === ""){
+//     num = 5;
+// }
+// for (table = 1; table<= 10 ; table++){
+//     let answer = num * table;
+//     document.write(`${num} x ${table} = ${answer}<br>`)
+// }
+
+
+
+//.......question 5.........
+// var subject1 = prompt("what's your first subject")
+// var subject2 = prompt("what's your second subject")
+// var subject3 = prompt("what's your third subject")
+
+
+// var obtainedMarks1 = parseInt(prompt("what's your obtained marks in subject 1"))
+// var obtainedMarks2 = parseInt(prompt("what's your obtained marks in subject 2"))
+// var obtainedMarks3 = parseInt(prompt( "what's your obtained marks in subject 3"))
+
+// var totalMarks = 100
+
+// var percentAge1 = parseInt((obtainedMarks1 / totalMarks) * 100)
+// var percentAge2 = parseInt((obtainedMarks2 / totalMarks) * 100)
+// var percentAge3 = parseInt((obtainedMarks3 / totalMarks) * 100)
+
+// let totalObtainedMarks = parseInt(obtainedMarks1 + obtainedMarks2 + obtainedMarks3);
+// // let totalPercent = parseInt(totalObtainedMarks / totalMarks) * 100
+// let totalPercent = 100
+
+// // document.write(`<b>SUBJECT &nbsp; TOTALMARKS &nbsp; OBTAINEDMARKS &nbsp; PERCENTAGE<b>`)
+// document.write(`<br> ${subject1} <br> ${totalMarks} <br>${obtainedMarks1} <br> ${percentAge1}`)
+// document.write(`<br> ${subject2} <br> ${totalMarks} <br>${obtainedMarks2} <br> ${percentAge2}`)
+// document.write(`<br> ${subject3} <br> ${totalMarks} <br>${obtainedMarks3} <br> ${percentAge3}`)
+// document.write(`<br>Total obtained marks : ${totalObtainedMarks}`)
+// document.write(`<br>Total Marks : ${totalMarks}`)
+// document.write(`<br>Total Percentage : ${totalPercent}`)
+
+
+
+
+
+
+
+
+
+
